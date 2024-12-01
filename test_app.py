a6a2329e363a7f69e6659042f302f7d170c722a3
striff2001 <striff2001@SFVM.myguest.virtualbox.org>
2024-12-01 20:32:51 +0300

    First create

---
+++ test_app.py
@@ -0,0 +1,13 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_hello_world(client):
+    """Проверка, что '/' возвращает 'Hello world!'"""
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data.decode() == "Hello world!"
